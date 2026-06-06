eb41b5c81c32533660bcfb105c547ff4119cc518
Ernesto Enrique Urrea Lopez <ernesto.urrea03@gmail.com>
2026-06-05 22:02:43 -0600

    App core module bringup

---
+++ components/app_core/app_core.c
@@ -0,0 +1,5 @@
+// components/app_core/app_core.c
+
+#include "app_core.h"
+
+
---
+++ components/app_core/include/app_core.h
@@ -0,0 +1,8 @@
+// components/app_core/include/app_core.h
+
+#ifndef APP_CORE_H
+#define APP_CORE_H
+
+
+
+#endif // APP_CORE_H
